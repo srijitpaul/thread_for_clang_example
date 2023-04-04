c30a54c5eb75f313888629fdc4e722014f56d151
Ryan Hill <rchrys.hill@gmail.com>
2023-04-04 12:14:01 +0100

    Simplify example, bug still reproduced

--- main.cpp
+++ main.cpp
@@ -1,30 +1,16 @@
-#include <Threads.h>
-#include <iostream>
-
 class TestClass
 {
 public:
-    int result=0;
+    void crash() {}
 
-    int sum(int a, int b, int c)
+    template<typename T>
+    void ompCrash()
     {
-        return a*b + c;
-    }
-
-    // Might be containers of types too...
-    template<class T, class V>
-    inline void runSum(T i, V j)
-    {
-        thread_for( i, j, 
+        #pragma omp parallel for schedule(static)
         {
-            for (int m = 0; m < 3; m++) 
-            {
-                for (int si = 0; si < m; si++) 
-                {
-                    result += sum(si, m, j);
-                }
-            }
-        })
+            for (uint64_t i=0; i<10; ++i) 
+                this->crash();
+        }
     }
 
 };
@@ -32,7 +18,7 @@ public:
 int main(int argc, char** argv)
 {
     TestClass t;
-    t.runSum(2, 10);
-    std::cout << t.result << std::endl; 
+    t.ompCrash<void>();
     return 0;
 }
+
